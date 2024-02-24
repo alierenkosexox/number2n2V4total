8b397a6e580045ba3eb1a313544044cfeb5f561c
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-24 17:04:27 +0300

    commnet and bug fixed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int number, total = 0;              // sayı ve toplam değerlerini tanır
+        Scanner input = new Scanner(System.in);     // scanner çağır
+
+        while (true) {              // tek sayı yazılana kadar sayı iste döngüsünü başlat
+            System.out.println("Sayı Giriniz : ");      // sayı iste
+            number = input.nextInt();           // verilen sayı datasını kaydet
+
+            if (number % 4 == 0) {       //çift ve 4'e tam bölünme işelmini belirle
+                total += number;                            // her sayı yazıldığında bunu topla
+
+            }
+            else if (number % 2 == 0) {
+                total += number;
+
+            }
+            else if (number % 2 == 1) {                   // tek sayı yazılırsa döngüyü bitir
+                break;
+            }
         }
+        System.out.println("Girdiğiniz Çift ve 4 e tam bölünen sayıların toplamı :" + total);   // toplamı yazdır
     }
-}
+} // Ali Eren KÖSE 24/02/2024   17:01
